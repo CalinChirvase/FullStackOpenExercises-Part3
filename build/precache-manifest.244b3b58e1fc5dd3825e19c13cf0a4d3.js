a521760bcbdcd7c6b69371e66267355a0f8e4a83
CalinChirvase <calin.chirvase@yahoo.com>
2021-01-09 22:30:49 -0500

    add frontend production build

---
+++ build/precache-manifest.244b3b58e1fc5dd3825e19c13cf0a4d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1442184322f9512b746be3844f75f7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c0f444ebba93378dbff",
+    "url": "/static/js/2.19ed25a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.19ed25a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8216fd79d4287899af92",
+    "url": "/static/js/main.9487e170.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const morgan = require('morgan')
 
 app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('data', (req, res) => {
     return JSON.stringify(req.body)
