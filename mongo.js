4924ea85dada84312f117be5c346d41a98bb2106
CalinChirvase <calin.chirvase@yahoo.com>
2021-01-12 16:56:26 -0500

    add exercises 3.9-3.12

---
+++ mongo.js
@@ -0,0 +1,47 @@
+//mongo "mongodb+srv://cluster0.eqfwu.mongodb.net/<dbname>" --username admin
+
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+  `mongodb+srv://admin:${password}@cluster0.eqfwu.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const personSchema = new mongoose.Schema({
+  id: Number,
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+    id: Math.floor(Math.random() * 1000),
+    name: name,
+    number: number,
+})
+
+
+if (process.argv.length === 5) {
+    person.save().then(result => {
+        console.log('person saved!')
+        mongoose.connection.close()
+    })
+} else if (process.argv.length === 3){
+    console.log('phonebook:')
+    Person.find({}).then(result => {
+        result.forEach(person => {
+          console.log(person.name, person.number)
+        })
+        mongoose.connection.close()
+      })
+}
